5654aba3ca58eb16d612732f891fc5fc88557960
garebare <garedaiki@outlook.jp>
2020-01-30 17:08:30 +0900

    fix

--- src/terminal/serch_view.rs
+++ src/terminal/serch_view.rs
@@ -3,15 +3,12 @@ extern crate termion;
 use std::io::{stdin, stdout, Write};
 use termion::event::{Event, Key};
 use termion::input::TermRead;
-use termion::raw::IntoRawMode;
-use termion::screen::AlternateScreen;
 
 use super::super::serch_parser::serch;
 use super::draw;
 
 pub fn views(result:&Vec<serch::Serch>) {
     let stdin = stdin();
-    let mut cols = 0;
     let mut stdout = stdout();
 
     write!(
@@ -22,7 +19,7 @@ pub fn views(result:&Vec<serch::Serch>) {
     .unwrap();
     stdout.flush().unwrap();
 
-    draw::serch_view(&mut stdout ,result, cols);
+    draw::serch_draw(&mut stdout ,result);
 
     for evt in stdin.events() {
         match evt.unwrap(){
@@ -30,18 +27,10 @@ pub fn views(result:&Vec<serch::Serch>) {
                 return;
             }
 
-            Event::Key(Key::Up) => {
-                cols += 1;
-            }
-
-            Event::Key(Key::Down) => {
-                cols -= 1;
-            }
-
             _ => {}
         }
 
-        draw::serch_view(&mut stdout ,result, cols);
+        draw::serch_draw(&mut stdout ,result);
         stdout.flush().unwrap();
     }
 }
